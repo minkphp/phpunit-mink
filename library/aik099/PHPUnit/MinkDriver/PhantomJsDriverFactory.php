d5f8331435f21c8ba22d5adebafcb5032af28011
Malte Blättermann <malte.blaettermann@gmail.com>
2016-02-01 04:30:49 +0100

    NEW: Added PhantomJsDriverFactory to project
    Also added the new Factory to the DriverFactoryRegistry in DIContainer

---
+++ library/aik099/PHPUnit/MinkDriver/PhantomJsDriverFactory.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * This file is part of the phpunit-mink library.
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @copyright Alexander Obuhovich <aik.bold@gmail.com>
+ * @link      https://github.com/aik099/phpunit-mink
+ */
+
+
+namespace aik099\PHPUnit\MinkDriver;
+
+
+use aik099\PHPUnit\BrowserConfiguration\BrowserConfiguration;
+use Behat\Mink\Driver\DriverInterface;
+
+class PhantomJsDriverFactory implements IMinkDriverFactory
+{
+
+	/**
+	 * Returns driver name, that can be used in browser configuration.
+	 *
+	 * @return string
+	 */
+	public function getDriverName()
+	{
+		return 'phantomjs';
+	}
+
+	/**
+	 * Returns default values for browser configuration.
+	 *
+	 * @return array
+	 */
+	public function getDriverDefaults()
+	{
+		return array(
+			'host' => 'localhost',
+			'driver' => 'phantomjs',
+			'driverOptions' => array(
+				'api_endpoint' => '/api'
+			),
+			'browserName' => 'phantomjs',
+			'port' => 8510
+		);
+	}
+
+	/**
+	 * Returns a new driver instance according to the browser configuration.
+	 *
+	 * @param BrowserConfiguration $browser The browser configuration.
+	 *
+	 * @return DriverInterface
+	 */
+	public function createDriver(BrowserConfiguration $browser)
+	{
+		if ( !class_exists('Zumba\Mink\Driver\PhantomJSDriver') ) {
+			throw new \RuntimeException(
+				'Install PhantomJSDriver in order to use phantomjs driver.'
+			);
+		}
+
+		$driverOptions = $browser->getDriverOptions();
+		return new \Zumba\Mink\Driver\PhantomJSDriver(sprintf("http://%s:%s%s", $browser->getHost(), $browser->getPort(), $driverOptions['api_endpoint']));
+	}
+
+}
