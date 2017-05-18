39d2916b44e16a2b9ba3d10fc075a20ff2e9af37
Johnny Charcosset <johnny@evolunium.fr>
2017-05-18 10:58:16 +0200

    Update docblock

--- library/aik099/PHPUnit/SkippingUnsupportedTestCase.php
+++ library/aik099/PHPUnit/SkippingUnsupportedTestCase.php
@@ -8,13 +8,24 @@ if (version_compare(\PHPUnit_Runner_Version::id(), '5.0.0', '>=')) {
     /**
      * Implementation of the skipping for UnsupportedDriverActionException for PHPUnit 5+
      *
-     * This code should be moved back to \Behat\Mink\Tests\Driver\TestCase when dropping support for
+     * This code should be moved back to \aik099\BrowserTestCase when dropping support for
      * PHP 5.5 and older, as PHPUnit 4 won't be needed anymore.
      *
+     * Class SkippingUnsupportedTestCase
+     *
      * @internal
+     *
+     * @package aik099\PHPUnit
      */
     class SkippingUnsupportedTestCase extends \PHPUnit_Framework_TestCase
     {
+        /**
+         * This method is called when a test method did not execute successfully.
+         *
+         * @param \Exception|\Throwable $e
+         *
+         * @return void
+         */
         protected function onNotSuccessfulTest($e)
         {
             if ($e instanceof UnsupportedDriverActionException) {
@@ -26,12 +37,23 @@ if (version_compare(\PHPUnit_Runner_Version::id(), '5.0.0', '>=')) {
     }
 } else {
     /**
+     * Class SkippingUnsupportedTestCase
+     *
      * Implementation of the skipping for UnsupportedDriverActionException for PHPUnit 4
      *
      * @internal
+     *
+     * @package aik099\PHPUnit
      */
     class SkippingUnsupportedTestCase extends \PHPUnit_Framework_TestCase
     {
+        /**
+         * This method is called when a test method did not execute successfully.
+         *
+         * @param \Exception $e Exception.
+         *
+         * @return void
+         */
         protected function onNotSuccessfulTest(\Exception $e)
         {
             if ($e instanceof UnsupportedDriverActionException) {
