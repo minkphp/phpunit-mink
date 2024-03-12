12f65043ad12e3c12dc560bb0e352b9bed3bfdb8
Alex <aik099@users.noreply.github.com>
2024-03-12 12:30:39 +0200

    Improve PhpStorm auto-complete for DI container

---
+++ library/.phpstorm.meta.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PHPSTORM_META {
+	override(\aik099\PHPUnit\Application::getObject(), map([
+		'session_factory' => \aik099\PHPUnit\Session\SessionFactory::class,
+		'session_strategy_factory' => \aik099\PHPUnit\Session\SessionStrategyFactory::class,
+		'session_strategy_manager' => \aik099\PHPUnit\Session\SessionStrategyManager::class,
+		'isolated_session_strategy' => \aik099\PHPUnit\Session\IsolatedSessionStrategy::class,
+		'shared_session_strategy' => \aik099\PHPUnit\Session\SharedSessionStrategy::class,
+		'remote_url' => \aik099\PHPUnit\RemoteCoverage\RemoteUrl::class,
+		'remote_coverage_helper' => \aik099\PHPUnit\RemoteCoverage\RemoteCoverageHelper::class,
+		'test_suite_factory' => \aik099\PHPUnit\TestSuite\TestSuiteFactory::class,
+		'regular_test_suite' => \aik099\PHPUnit\TestSuite\RegularTestSuite::class,
+		'browser_test_suite' => \aik099\PHPUnit\TestSuite\BrowserTestSuite::class,
+		'driver_factory_registry' => \aik099\PHPUnit\MinkDriver\DriverFactoryRegistry::class,
+		'browser_configuration_factory' => \aik099\PHPUnit\BrowserConfiguration\BrowserConfigurationFactory::class,
+	]));
+}
